2f5ae8918e36ec392eb510b07bd7114e2133d86f
Jean-Fei Taing <jeanfei.taing@gmail.com>
2017-11-15 10:55:25 +0100

    Start implementation of Umami styleguide v2

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+// Requires
+var gulp = require('gulp');
+
+// Include plugins
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var minifycss = require('gulp-minify-css');
+// var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./"
+    });
+    
+    gulp.watch("sass/*.scss", ['sass']);
+    gulp.watch("sass/config/*.scss", ['sass']);
+    gulp.watch("sass/partials/*/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("sass/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
